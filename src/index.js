929ea0e9c85519391bf9e6499448d9e0b346f461
Jackie098 <carlosmiranda19122@gmail.com>
2020-04-14 01:00:24 -0300

    adding componentization

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const soma = (a, b) => {
-  return a + b;
-}
+import React from 'react';
+import { render } from 'react-dom';
 
-console.log(soma(10, 3));
+import App from './App';
+
+render(<App />, document.getElementById('app'));
