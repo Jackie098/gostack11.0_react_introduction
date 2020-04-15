4598f7a7a84cc65271ce5e5ffde4803180c6b80a
Jackie098 <carlosmiranda19122@gmail.com>
2020-04-14 22:08:54 -0300

    listing API projects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import './App.css';
-import backgroundImage from './assets/background.jpeg';
 
 import Header from './components/Header';
 
 function App(){
-  const [projects, setProjects] = useState(['alguma coisa', 'ota coisa']);
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    }) ;
+  }, []);
 
   function handleAddProject() {
     // projects.push(`Novo projeto ${Date.now()}`);
@@ -19,13 +25,12 @@ function App(){
   return (
     <>
       <Header title="Projects" />
-      <img width={250} src={backgroundImage}></img>
+
       <ul>
-        {projects.map(project => <li key={project}>{project}</li>)}
+        {projects.map(project => <li key={project.id}>{project.title}</li>)}
       </ul>
 
       <button type="button" onClick={handleAddProject}>Tip to add</button>
-    
       
     </>
   );
